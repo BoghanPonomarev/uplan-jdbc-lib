e622f13d2ad3694b9def6bf78c28321d93b65ea0
Bohdan Ponomarev <bponomarev@varteq.com>
2019-12-23 10:28:10 +0200

    Add utility sqlTimeUtils

---
+++ src/main/java/com/uplan/jdbc/util/SqlTimeUtils.java
@@ -0,0 +1,37 @@
+package com.uplan.jdbc.util;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class SqlTypeUtils {
+
+  private SqlTypeUtils() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static Timestamp nullableTimestamp(LocalDateTime localDateTime) {
+    if (localDateTime != null) {
+      return Timestamp.valueOf(localDateTime);
+    }
+    return null;
+  }
+
+  public static Date nullableDate(LocalDate localDate) {
+    if (localDate != null) {
+      return Date.valueOf(localDate);
+    }
+    return null;
+  }
+
+  public static Timestamp currentTimestamp() {
+    return nullableTimestamp(LocalDateTime.now());
+  }
+
+  public static Date currentDate() {
+    return Date.valueOf(LocalDate.now());
+  }
+
+}
